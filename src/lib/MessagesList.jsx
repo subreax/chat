9aba27e4c4cc91b04ab10ce8cc2d6b0d8f06b854
subreax <extode@gmail.com>
2022-12-05 11:05:30 +0300

    Changed some colors. Auto-scroll to bottom of the messages list

--- src/lib/MessagesList.jsx
+++ src/lib/MessagesList.jsx
@@ -1,20 +1,40 @@
+import React from "react";
 import Message from "./Message";
 
-const MessagesList = ({ messages, myUsername }) => {
-    return (
-        <div className="messages__container-root">
-            <div className="messages__container">
-                {messages.map((message) => {
-                    return (
-                        <Message
-                            key={message.id}
-                            message={message}
-                            isMe={message.username === myUsername}
-                        />
-                    );
-                })}
+/*const MessagesList = ({ messages, myUsername }) => {
+    
+};*/
+
+class MessagesList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.containerRef = React.createRef();
+    }
+
+    render() {
+        return (
+            <div className="messages__container-root" ref={this.containerRef}>
+                <div className="messages__container">
+                    {this.props.messages.map((message) => {
+                        return (
+                            <Message
+                                key={message.id}
+                                message={message}
+                                isMe={
+                                    message.username === this.props.myUsername
+                                }
+                            />
+                        );
+                    })}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+
+    componentDidUpdate() {
+        const el = this.containerRef.current;
+        el.scrollTo(0, el.scrollHeight);
+    }
+}
+
 export default MessagesList;
